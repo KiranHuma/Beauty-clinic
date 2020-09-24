e26fafbb5ad4c7541d945ac8c579dc7dec682612
Aimal Huma <66478525+KiranHuma@users.noreply.github.com>
2020-09-24 14:16:26 +0500

    database attach

--- servicesfrm1/Reports/pruductreport/productreporfrm.vb
+++ servicesfrm1/Reports/pruductreport/productreporfrm.vb
@@ -143,6 +143,7 @@ Public Class productreporfrm
 
     Private Sub productreporfrm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         wordconvert()
+
     End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
