20acf813a046999a845d08dab1747fbcfb1d165d
Aimal Huma <66478525+KiranHuma@users.noreply.github.com>
2020-10-13 17:07:02 +0800

    startup login form

--- servicesfrm1/My Project/Application.Designer.vb
+++ servicesfrm1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.servicesfrm1.MainFrom1
+            Me.MainForm = Global.servicesfrm1.loginfrm
         End Sub
     End Class
 End Namespace
